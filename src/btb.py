76849bcf5653c987a56cb222d54e1808cb2009a4
Jitender Jangra <jitenderkumarjangra149@gmail.com>
2025-04-18 04:47:45 +0530

    1 bit dynamic predictor added

--- src/btb.py
+++ src/btb.py
@@ -1,7 +1,6 @@
 # If the instruction is jal/jalr, we are always making the prediction as 'true'.
 # Otherwise, we are making the prediction 'true' if we are going back and 'false' if we are going forward.
 
-# Make this 1 bit dynamic predictor and write the function for this.
 class BTB:
     def __init__(self):
         self.table={} # initialize an empty dictionary to store the branch prediction table
@@ -26,5 +25,29 @@ class BTB:
         return self.table[pc][1]
     
 
-
+# # 1 bit dynamic predictor
+# class BTB:
+#     def __init__(self):
+#         self.table = {}  # initialize an empty dictionary to store the branch prediction table
     
+#     def find(self, pc):  # finding the branch prediction table entry for the given PC address
+#         if pc in self.table.keys():  # if the PC address is present in the table
+#             return True  # return true
+#         return False  # return false
+    
+#     def enter(self, is_jump, pc, next_address):  # adding a new entry to the branch prediction table
+#         if not self.find(pc):
+#             # If this is a new entry, initialize based on the current branch outcome
+#             self.table[pc] = [is_jump, next_address]
+#         else:
+#             # If entry exists, update the prediction bit based on actual outcome
+#             # This is the 1-bit dynamic predictor logic - it flips the prediction
+#             # bit based on the most recent branch outcome
+#             self.table[pc][0] = is_jump
+#             self.table[pc][1] = next_address
+    
+#     def predict(self, pc):
+#         return self.table[pc][0]
+    
+#     def next_add(self, pc):
+#         return self.table[pc][1]
